2b1f11e679be1d9e8a97ee550c81a33842aec176
DLDiMar <81651316+DLDiMar@users.noreply.github.com>
2021-11-08 14:10:00 -0700

    Update app.controller.ts
    Updated Post

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,7 @@ import {
   Controller,
   Get,
   Param,
-  Put,
+  Patch,
   Post,
   Body,
   BadRequestException,
@@ -11,6 +11,7 @@ import { AppService } from './app.service';
 import { TvShow } from '../entities/tv_show.entity';
 import { ApiTags, ApiOkResponse, ApiResponse } from '@nestjs/swagger';
 import { CreateTvShowDto } from '../dto/create-tv_show.dto';
+import { UpdateTvShowDto } from '../dto/update-tv_show.dto';
 
 @ApiTags('TvShows')
 @Controller()
@@ -26,9 +27,6 @@ export class AppController {
   @ApiOkResponse({ type: TvShow, isArray: true })
   @Get(':id')
   findOneShow(@Param('id') id: string): TvShow {
-    if (id === '') {
-      throw new BadRequestException();
-    }
     return this.appService.findOneShow(Number(id));
   }
 
@@ -45,34 +43,9 @@ export class AppController {
     }
     return this.appService.createTvShow(createTvShowDto);
   }
-  // Below (commented out) is trial update tv show by id
-  /*
   @ApiOkResponse({ type: TvShow, isArray: true })
-  @Put('/change/:id')
-  updateTvShow(
-    @Param('id') id: number,
-    @Param('series_name') series_name: string,
-    @Param('air_date') air_date: string,
-    @Param('number_seasons') number_seasons: number,
-    @Param('number_episodes') number_episodes: number,
-  ): TvShow {
-    if (!id || !series_name || !number_episodes || !number_seasons) {
-      throw new BadRequestException();
-    }
-    return this.appService.updateTvShow(
-      id,
-      series_name,
-      air_date,
-      number_seasons,
-      number_episodes,
-    );
-  }
-  */
-
-  //Placeholder Put controller (see above for actual)
-  @ApiOkResponse({ type: TvShow, isArray: true })
-  @Put('/change/:id')
-  updateTvShow() {
-    return this.appService.updateTvShow();
+  @Patch('/change/:id')
+  updateTvShow(@Param('id') id: number, updateTvShowDto: UpdateTvShowDto) {
+    return this.appService.updateTvShow(Number(id), updateTvShowDto);
   }
 }
